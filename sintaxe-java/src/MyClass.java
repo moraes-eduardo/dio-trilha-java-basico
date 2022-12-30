da7487554fa3bdf5a01c8cfd696cca07c698c8c7
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-30 13:46:32 -0300

    inclusão exemplo classes

---
+++ sintaxe-java/src/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass {
+    
+    public static void main (String [] args ) {
+        String firstName = "Eduardo";
+        String secondName = "Silva";
+
+        String fullName = fullName (firstName, secondName);
+
+        System.out.println(fullName);
+    }
+
+    public static String fullName (String firstName, String secondName){
+        return "Method Result: " + firstName.concat(" ").concat(secondName);
+    }
+
+}
