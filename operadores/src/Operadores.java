a0ec12d2d6a75872d93470ade31a1dcbd27a8180
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-30 13:37:20 -0300

    inclusão exemplos

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,5 +1,38 @@
+//import java.sql.Date;
+
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Operadores atribuição
+        String nome = "GLEYSON";
+        int idade = 22;
+        double peso = 68.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        //Date dataNascimento = new Date();
+
+        //Operadores aritmeticos
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        String nomeCompleto = "LINGUAGEM" + "JAVA"; // concatenação
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?"; 
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        
     }
 }
