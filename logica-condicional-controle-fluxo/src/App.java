228aa4ed57450331fc33d6f23df9f10b1a207f45
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-31 12:11:49 -0300

    Adição nova aula

---
+++ logica-condicional-controle-fluxo/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println(1 == 2);
+        System.out.println(4 != 5);
+        System.out.println(2 > 5);
+        System.out.println(6 >= 6);
+        System.out.println(5 < 2);
+        System.out.println(2 <= 2);
+
+        
+    }
+}
