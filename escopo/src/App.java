c43cebfd85217d03a51c09b668503f2aa8d53e02
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-31 09:29:29 -0300

    ajustes projeto escopo

---
+++ escopo/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        // dentro do método - limitado ao método
+        // dentro da classe - em toda a classe
+        // dentro do bloco(for, if..) - limitado ao bloco
+
+
+
+
+    }
+}
