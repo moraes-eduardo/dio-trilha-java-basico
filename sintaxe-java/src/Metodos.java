fddf346494f0a17c1f59f6661325a53f41e4756e
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-31 08:19:49 -0300

    definições de métodos

--- sintaxe-java/src/Metodos.java
+++ sintaxe-java/src/Metodos.java
@@ -1,6 +1,12 @@
+// Classes são definidas por atributos e métodos
+// Atributos são variáveis de diferentes tipos e valores
+// Métodos são funções ou sub-rotinas disponíveis dentro de nossas classes
+
 public class Metodos {
     public static void main(String[] args) {
         
+        
+
         
 
     }
