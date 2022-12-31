89cb257440928d3d9e33e5b01afaf9652d46a7e8
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-31 09:14:20 -0300

    Inclusão de novos métodos

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,9 +3,40 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
-        System.out.println(smartTv.ligada);
-        System.out.println(smartTv.canal);
-        System.out.println(smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.out.println("Volume Atual ? " + smartTv.volume);
+
+        System.out.println("------------------------");
+
+        smartTv.ligar();
+        System.out.println("Novo Status - TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status - TV Ligada ? " + smartTv.ligada);
+
+        System.out.println("------------------------");
+
+        smartTv.aumentarCanal();
+        System.out.println("Novo Status - Canal Atual ? " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+        System.out.println("Novo Status - Canal Atual ? " + smartTv.canal);
+
+        System.out.println("------------------------");
+
+        smartTv.aumentarVolume();
+        System.out.println("Novo Status - Volume Atual ? " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        System.out.println("Novo Status - Volume Atual ? " + smartTv.volume);
 
     }
 }
