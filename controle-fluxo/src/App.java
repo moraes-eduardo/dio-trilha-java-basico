b54e94fedd01e9669e05166bf901a45e7e4c5ec8
moraes-eduardo <moraes-eduardo@hotmail.com>
2022-12-31 13:03:27 -0300

    inclusão exemplos

--- controle-fluxo/src/App.java
+++ controle-fluxo/src/App.java
@@ -1,6 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        int numero = 20;
+        if (numero < 25){
+            System.out.println("Número menor que 25");
+        }
+
+        int valor = 1000;
+        if (valor < 1000){
+            System.out.println("Valor menor que 1000");
+        } else {
+            System.out.println("Valor maior ou igual a 1000");
+        }
+
+
         
     }
 }
